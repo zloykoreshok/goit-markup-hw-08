cb18bab523600903a90abefa55321da94f5d0f98
zloykoreshok <daodzen45@gmail.com>
2022-06-14 22:59:09 +0300

    header modal added

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,9 @@
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
+      document.body.classList.toggle("modal-open");
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  
